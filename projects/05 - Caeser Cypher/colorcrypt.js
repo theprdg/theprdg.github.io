b06613ea673457aaf185c0a957d8ff3b1adbeb71
Shawn Huynh <theprdg@gmail.com>
2018-01-26 10:46:56 -0500

    Accuracy Improvement
    - reformatted and refactored code
    - fixed incorrect output encrypting 'z' to a space; results in no spaces being registered in the output

--- projects/05 - Caeser Cypher/colorcrypt.js
+++ projects/05 - Caeser Cypher/colorcrypt.js
@@ -1,5 +1,20 @@
-var pw = "caesar", keyCount = 0, login = "",
-	encryptMsg = '', decryptMsg = '', final = '';
+var pw = "caesar", 
+	keyCount = 0, 
+	login = "",
+	encryptMsg = '', 
+	decryptMsg = '', 
+	message = '',
+	final = '',
+	e = $("#encryptbtn")[0],
+	d = $("#decryptbtn")[0],
+	status;
+
+e.addEventListener("click",function(){
+	status = 1;
+});
+d.addEventListener("click",function(){
+	status = 2;
+});
 
 $("#container").hide();
 $("#container2").hide();
@@ -10,66 +25,72 @@ $(window).resize(function(){
 }).resize();
 
 $(window).keypress(function(event){
-	login += String.fromCharCode(event.which);
+	login += String.fromCharCode(event.which),
+	keyCount++;
 	var angle = Math.floor(Math.random()*360)+1;
+
+	$("body").css("background",randomColor()) //fallback for older browsers
+	.css("background","-webkit-linear-gradient("+ angle +"deg, " + randomColor() + ", " + randomColor() + ")")
+	.css("background","linear-gradient("+ angle +"deg, " + randomColor() + ", " + randomColor() + ")");	
+
 	if (login === pw) {
-		$("body").css("background","#eff0f1");
-		$("body").css("background","-webkit-linear-gradient(0deg, #e0e0e0, #e0e0e0");
-		$("body").css("background","linear-gradient(0deg, #e0e0e0, #e0e0e0");
 		$(this).unbind("keypress");
 		$("#container").fadeIn(500);
 		$("#container2").fadeIn(500);
 	}
-	else if (keyCount >= pw.length){
+	else if (keyCount == pw.length){
 		keyCount = 0;
 		login = '';
 	}
-	else {
-		$("body").css("background",randomColor()); //fallback for older browsers
-		$("body").css("background","-webkit-linear-gradient("+ angle +"deg, " + randomColor() + ", " + randomColor() + ")");
-		$("body").css("background","linear-gradient("+ angle +"deg, " + randomColor() + ", " + randomColor() + ")");	
-	}
-	keyCount++;
 });
 
 function randomColor() {
-	return "#"+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+	return "#"+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 }
 
-$("#encryptbtn").click(encrypt);
-$("#decryptbtn").click(decrypt);
+$("#encryptbtn").click(crypt);
+$("#decryptbtn").click(crypt);
 
-function encrypt() {
+function crypt() {
 	final = '';
-	encryptMsg = $("textarea#encrypt").val();
-	for(var i = 0; i < encryptMsg.length; i++) {
-		if(encryptMsg[i].charCodeAt() == 10 || encryptMsg[i].charCodeAt() == 32) {
-			final += String.fromCharCode(32);
-		}
-		else if (encryptMsg[i].charCodeAt()+5 > 126) {
-			final += String.fromCharCode(encryptMsg[i].charCodeAt()+5 - 95);
-		}
-		else {
-			final += String.fromCharCode(encryptMsg[i].charCodeAt()+5);
-		}
+
+	if (status == 1) {
+		message = $("#encrypt").val();
 	}
-	$("#output").text(final);
-}
+	else {
+		message = $("#decrypt").val();
+	}
 
-function decrypt() {
-	final = '';
-	decryptMsg = $("textarea#decrypt").val();
-	for(var i = 0; i < decryptMsg.length; i++) {
-		if(decryptMsg[i].charCodeAt() == 10 || decryptMsg[i].charCodeAt() == 32) {
+	for(var i = 0; i < message.length; i++) {
+		if(message[i].charCodeAt() == 10) {
 			final += String.fromCharCode(32);
 		}
-		else if (decryptMsg[i].charCodeAt()-5 < 32) {
-			final += String.fromCharCode(decryptMsg[i].charCodeAt()-5 + 95);
+		else if (status == 1) {
+			if (message[i].charCodeAt()+5 > 126) {
+				if (message[i].charCodeAt()+5-95 == 32) {
+					final += '\xa0';
+				}
+				else {
+					final += String.fromCharCode(message[i].charCodeAt()+5 - 95);
+				}
+			}
+			else {
+				final += String.fromCharCode(message[i].charCodeAt()+5);
+			}
 		}
 		else {
-			final += String.fromCharCode(decryptMsg[i].charCodeAt()-5);
+			if (message[i].charCodeAt()-5 < 32) {
+					final += String.fromCharCode(message[i].charCodeAt()-5 + 95);
+			}
+			else {
+				if (message[i].charCodeAt()-5 == 32) {
+					final += '\xa0';
+				}
+				else {
+					final += String.fromCharCode(message[i].charCodeAt()-5);
+				}
+			}
 		}
-		console.log(final[i].charCodeAt());
 	}
 	$("#output").text(final);
 }
